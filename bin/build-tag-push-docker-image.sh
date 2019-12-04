fe7db8d645613d32f7fb432aba5a79bc4d89b40a
XhorxhinaT <56897059+XhorxhinaT@users.noreply.github.com>
2019-12-04 23:07:12 +0100

    Update build-tag-push-docker-image.sh
    building and pushing  docker image when you already have it locally saved

--- bin/build-tag-push-docker-image.sh
+++ bin/build-tag-push-docker-image.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+#Build an image from a Dockerfile
+#ether-node/bootnode/sealernode
+#$ docker  build /[User_local_location]/ether-node --tag ether-node
+#$ gcloud auth configure-docker
+#$ docker tag ether-node gcr.io/totemic-carrier-259013/ether-node 
+#$ gcloud docker push gcr.io/totemic-carrier-259013/ether-node
+
+
+
+
+
 #TODO write a script that will push all our docker images to GC so we can use them
